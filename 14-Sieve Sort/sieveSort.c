68d162d42ffd78b258f23772f4eb18eb1b849bca
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-02-16 00:25:09 +0530

    tried sieve sort

--- 14-Sieve Sort/sieveSort.c
+++ 14-Sieve Sort/sieveSort.c
@@ -1,27 +1,105 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 struct subArr {
-    
+    int key;
+
+    struct subArr* prev;
+    struct subArr* next;
 };
 
-/**
- * method for sieve sort
- * @param arr   the input array
- * @param size  size of the input array
- * @param dir   direction of sieveing
- * 
- * dir = 0 => left to right
- * dir = 1 => right to left
-*/
-void sort(int arr[], int size, int dir)
-{
-    if(dir == 0)
-    {
-        int reference = arr[0];
-        for(int i = 1; i < size; i++)
-        {
+struct subArrList {
+    struct subArr* list;
+    struct subArrList* next;
+};
 
+struct subArr* createNode(int key, struct subArr* prev) {
+    struct subArr* term = (struct subArr*) malloc(sizeof(struct subArr));
+
+    if(prev != NULL)    prev -> next = term;
+
+    term -> key = key;
+    term -> prev = prev;
+    term -> next = NULL;
+
+    return term;
+}
+
+struct subArrList* createNodeList(struct subArr* node) {
+    struct subArrList* term = (struct subArrList*) malloc(sizeof(struct subArrList));
+
+    term -> list = node;
+    term -> next = NULL;
+
+    return term;
+}
+
+void displayList(struct subArr* head) {
+    while(1) {
+        printf("%d", head -> key);
+
+        if(head -> next == NULL)
+            break;
+        
+        printf("->");
+        head = head -> next;
+    }
+    printf("\n");
+}
+
+// dir = 0 => left to right, dir = 1 => right to left
+void sort(struct subArrList* head, int dir) {
+    struct subArrList* iterator = head;
+    struct subArrList* newHead = NULL;
+    struct subArrList* newIterator;
+
+    while(1) {
+        struct subArr* list = iterator -> list;
+        struct subArr* newList;
+
+        if(dir == 0) {
+            int comp = list -> key;
+            list = list -> next;
+
+            while(list != NULL) {
+                if(list -> key < comp) {
+
+                }
+            }
         }
     }
+}
+
+int main() {
+    int size;
+    printf("Enter the number of elements: ");
+    scanf("%d", &size);
+
+    struct subArr* head;
+    struct subArr* iterator = NULL;
+
+    bool isHeadAssigned = false;
+    printf("Enter the elements: \n");
+    for(int i = 0; i < size; i++) {
+        int key;
+        scanf("%d", &key);
+
+        struct subArr* term = createNode(key, iterator);
+
+        if(!isHeadAssigned) {
+            head = term;
+            iterator = term;
+
+            isHeadAssigned = true;
+            continue;
+        }
+
+        iterator = term;
+    }
+
+    printf("The list: \n");
+    displayList(head);
+
+    return 0;
 }
