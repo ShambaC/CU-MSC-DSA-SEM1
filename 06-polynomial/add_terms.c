83b27cfe732fa4c2e894304900e2da480f9c80fa
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-03-06 18:38:38 +0530

    removed sorting

--- 06-polynomial/add_terms.c
+++ 06-polynomial/add_terms.c
@@ -73,8 +73,8 @@ int* sum(int* A, int* B, int sizeA, int sizeB)
 void sortPoly(int* poly, int size) {
     size *= 2;
     for(int i = 1; i < size - 1; i+=2) {
-        for(int j = 1; j < size - i - 1; j+=2) {
-            if(poly[i] > poly[i+2]) {
+        for(int j = 1; j < size - i - 2; j+=2) {
+            if(poly[i] < poly[i+2]) {
                 int tmpCoeff = poly[i-1];
                 int tmpPow = poly[i];
                 poly[i-1] = poly[i+1];
@@ -119,13 +119,13 @@ int main()
     int* res = sum(expA, expB, m, n);
 
     printf("\nPolynomial 1: ");
-    sortPoly(expA, m);
+    // sortPoly(expA, m);
     displayPoly(expA, m);
     printf("\nPolynomial 2: ");
-    sortPoly(expB, n);
+    // sortPoly(expB, n);
     displayPoly(expB, n);
     printf("\nSum: ");
-    sortPoly(res, m+n);
+    // sortPoly(res, m+n);
     displayPoly(res, m+n);
 
     return 0;
